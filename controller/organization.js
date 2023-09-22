f5d3a8af9151a063eed97a48c1aff2f51780ba42
DinhTu <dinhqtu861@gmail.com>
2023-09-22 10:54:45 +0700

    change name organization

---
+++ controller/organization.js
@@ -0,0 +1,198 @@
+const Organization = require("../model/organization");
+const OrganizationGroup = require("../model/organizations_groups");
+const Partnership = require("../model/partnership");
+
+const getAllOrganization = (req, res) => {
+  Organization.find({})
+    .then((data) => {
+      res.status(200).json({ success: true, data: data });
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false, message: err });
+    });
+};
+
+const getAllPartnerByUserId = (req, res) => {
+  Partnership.find({ user: req.params.id })
+    .then((data) => {
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false, message: err });
+    });
+};
+
+const getAllProjectByPartnerId = (req, res) => {
+  ProjectPartner.find({ partner: req.params.id })
+    .populate({ path: "project", model: "Project" })
+    .then((data) => {
+      let transformedData = new Array();
+      transformedData = data.map((obj) => {
+        return {
+          ...transformedData,
+          _id: obj.project._id,
+          long_name: obj.project.long_name,
+          name: obj.project.name,
+          description: obj.project.description,
+          created: obj.project.created,
+        };
+      });
+      res.status(200).json({
+        success: true,
+        data: transformedData,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false, message: err });
+    });
+};
+
+const createOrganization = (req, res) => {
+  const newOrganization = new Organization();
+  newOrganization.name = req.body.name;
+  newOrganization.description = req.body.description;
+  newOrganization.created = new Date().getTime();
+  newOrganization
+    .save()
+    .then((data) => {
+      res.status(201).json({
+        success: true,
+        data: data,
+      });
+    })
+    .catch((error) =>
+      res.status(404).json({
+        success: false,
+        message: error,
+      })
+    );
+};
+
+const deleteOrganization = (req, res) => {
+  Organization.deleteOne({ _id: req.params.id }).then((data) => {
+    res
+      .status(200)
+      .json({
+        success: true,
+        data: data,
+      })
+      .catch((error) => {
+        res.status(404).json({
+          success: false,
+          message: error,
+        });
+      });
+  });
+};
+
+const createPartnership = (req, res) => {
+  const newPartnership = new Partnership();
+  newPartnership.projectId = req.body.projectId;
+  newPartnership.owner = req.body.owner;
+  newPartnership
+    .save()
+    .then((data) => {
+      res.status(200).json({ success: true, data: data });
+    })
+    .catch((error) => {
+      res.status(404).json({ success: false, message: error });
+    });
+};
+
+const addUserToPartner = (req, res) => {
+  const newUserInPartner = new Partnership();
+  newUserInPartner.user = req.body.user;
+  newUserInPartner.partner = req.body.partner;
+  newUserInPartner
+    .save()
+    .then((data) => {
+      res.status(200).json({ success: true, data: data });
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false, message: err });
+    });
+};
+
+const removeProjectFromPartner = (req, res) => {
+  ProjectPartner.deleteOne({ _id: req.params.id })
+    .then((data) => {
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        success: false,
+        message: err,
+      });
+    });
+};
+
+const removeUserFromPartner = (req, res) => {
+  Partnership.deleteOne({ _id: req.params.id })
+    .then((data) => {
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        success: false,
+        message: err,
+      });
+    });
+};
+
+const createProjectWithPartner = async (req, res) => {
+  const project = new Project();
+
+  project.name = req.body.name;
+  project.description = req.body.description;
+  project.created = new Date().getTime();
+
+  await project
+    .save()
+    .then(async (data) => {
+      const partner_project = new PartnerProject();
+      partner_project.partner = req.params.id;
+      partner_project.project = data._id;
+      await partner_project
+        .save()
+        .then(() => {
+          res.status(201).json({
+            success: true,
+            message: "Project added partner",
+          });
+        })
+        .catch((err) => {
+          res.status(404).json({
+            success: false,
+            message: err,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        success: false,
+        message: err,
+      });
+    });
+};
+
+module.exports = {
+  getAllOrganization,
+  createOrganization,
+  deleteOrganization,
+  createPartnership,
+  addUserToPartner,
+  removeProjectFromPartner,
+  removeUserFromPartner,
+  getAllPartnerByUserId,
+  getAllProjectByPartnerId,
+  createProjectWithPartner,
+};
--- model/partnership.js
+++ model/partnership.js
@@ -3,12 +3,8 @@ const { Schema } = mongoose;
 
 const partnership = new Schema(
   {
-    organizationId: {
-      type: Schema.Types.ObjectId,
-      ref: "organizations",
-      require: true,
-    },
-    userId: { type: Schema.Types.ObjectId, ref: "user", require: true },
+    project: { type: Schema.Types.ObjectId, ref: "user", require: true },
+    owner: { type: Schema.Types.ObjectId, ref: "user", require: true },
     collaborator: [],
   },
   { versionKey: false }
